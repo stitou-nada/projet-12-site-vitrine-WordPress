50a2c7a9d34154b21bffc307e11b4cae4fe00a81
nada stitou <nadastito1087@gmail.com>
2022-04-21 00:23:42 +0000

    configuration

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -92,5 +92,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
 }
 
+/**importer */
+@ini_set( 'upload_max_filesize' , '128M' );
+@ini_set( 'post_max_size', '128M');
+@ini_set( 'memory_limit', '256M' );
+@ini_set( 'max_execution_time', '300' );
+@ini_set( 'max_input_time', '300' );
+
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+
